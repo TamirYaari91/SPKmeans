f7d65dd375856c6cbb0c45500d8c0f74ec2bcf10
TamirYaariTAU <73110585+TamirYaariTAU@users.noreply.github.com>
2021-08-19 14:13:00 +0300

    Add files via upload

--- spkmeansmodule.c
+++ spkmeansmodule.c
@@ -1,6 +1,6 @@
 #define PY_SSIZE_T_CLEAN  /* For all # variants of unit formats (s#, y#, etc.) use Py_ssize_t rather than int. */
 
-#include <python2.7/Python.h>
+#include <python3.4/Python.h>
 
 /*#include <Python.h>*/       /* MUST include <Python.h>, this implies inclusion of the following standard headers:
                              <stdio.h>, <string.h>, <errno.h>, <limits.h>, <assert.h> and <stdlib.h> (if available). */
